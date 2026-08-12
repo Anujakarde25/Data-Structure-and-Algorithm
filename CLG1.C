bb74cf49e0622fa512cd83508c7d6efdcb352d4e
Anujakarde25 <anujakarde6256@gmail.com>
2026-08-12 22:51:16 +0530

    Add files via upload

---
+++ CLG1.C
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int i,a[50],b[50],n;
+ clrscr();
+ printf("enter limit:");
+ scanf("%d",&n);
+ printf("enter 1st array elements:");
+ for(i=0; i<n; i++)
+ {
+  scanf("%d",&a[i]);
+ }
+ for(i=0; i<n; i++)
+ {
+ b[i]=a[i]*a[i];
+ }
+ printf("simple array=\n");
+ for(i=0; i<n; i++)
+ {
+ printf(" %d",a[i]);
+ }
+ printf("\nsquare of array=\n");
+ for(i=0; i<n; i++)
+ {
+  printf(" %d",b[i]);
+ }
+ getch();
+}
